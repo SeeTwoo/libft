b310e08ced46cd6d9516591def89c2a5f42c7922
Walter Beschon <wbeschon@paul-f4Ar7s6.clusters.42paris.fr>
2025-01-28 21:50:11 +0100

    strtok is now awesome

--- ft_strtok.c
+++ ft_strtok.c
@@ -6,7 +6,7 @@
 /*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 23:47:46 by walter            #+#    #+#             */
-/*   Updated: 2025/01/27 14:48:21 by wbeschon         ###   ########.fr       */
+/*   Updated: 2025/01/28 21:48:35 by wbeschon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,42 @@
 char	*ft_strtok(char *s, char *sep)
 {
 	static char	*current = NULL;
-	static int	s_len;
-	static int	i;
 
 	if (!s && !current)
 		return (NULL);
 	if (s)
-	{
 		current = s;
-		s_len = (int)ft_strlen(s);
-		i = 0;
-	}
-	if (!current[i])
-		i++;
-	i += (int)ft_strspn(&current[i], sep);
-	if (i >= s_len)
-		return (current = NULL, NULL);
-	s = &current[i];
-	i += (int)ft_strcspn(&current[i], sep);
-	if (i < s_len)
-		current[i] = '\0';
+	current += ft_strspn(current, sep);
+	if (*current)
+		s = current;
+	else
+		return (current = NULL);
+	current += ft_strcspn(current, sep);
+	if (*current)
+		*current++ = '\0';
+	else
+		current = NULL;
 	return (s);
 }
 
 /*int	main(void)
 {
-	char	s[] = "hello, World ! How are you doing ?";
-	char	s2[] = "Hey, very good indeed ! And you ?";
+	char	s[] = "   hello,World ! How are you doing ?    ";
+	char	s2[] = "  ";
 	char	*token;
 
 	printf("null return hopefully : %s\n", ft_strtok(NULL, " "));
-	token = ft_strtok(s, " ");
+	token = ft_strtok(s, " ,");
 	while (token)
 	{
 		printf("%s\n", token);
-		token = ft_strtok(NULL, " ");
+		token = ft_strtok(NULL, " ,");
 	}
+	token = ft_strtok(NULL, " ");
+	printf("\n%s\n\n", token);
 	token = ft_strtok(s2, " ");
+	if (!token)
+		printf("girafe\n");
 	while (token)
 	{
 		printf("%s\n", token);
