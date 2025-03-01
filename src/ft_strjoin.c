e96848577b6da76d22b6f6383c9d85c50fbcc30e
Walter Beschon <wbeschon@paul-f4Ar8s2.clusters.42paris.fr>
2025-03-01 17:28:34 +0100

    strjoin is now an absolute banger

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: wbeschon <wbeschon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 12:03:50 by wbeschon          #+#    #+#             */
-/*   Updated: 2024/11/25 14:28:36 by wbeschon         ###   ########.fr       */
+/*   Updated: 2025/03/01 17:27:58 by wbeschon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*dest;
+	char	*temp;
+	size_t	len_s1;
 
-	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (dest == NULL)
+	if (!s1 || !s2)
 		return (NULL);
-	ft_strlcpy(dest, s1, ft_strlen(s1) + ft_strlen(s2) + 1);
-	ft_strlcat(dest, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	len_s1 = ft_strlen(s1);
+	dest = malloc(sizeof(char) * (len_s1 + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	ft_strcpy(dest, s1);
+	temp = dest + len_s1;
+	ft_strcpy(temp, s2);
 	return (dest);
 }
 
